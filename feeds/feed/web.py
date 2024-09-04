e94231885d3717f043f7197cc2ce850e43c6e673
Daniel <d@stuhrs.dk>
2024-09-04 10:46:51 +0200

    Refactor web checkers

--- feeds/feed/web.py
+++ feeds/feed/web.py
@@ -22,6 +22,7 @@ class ConfigKeys(StrEnum):
 
 
 class WebCheckerBase(FeedChecker):
+    """Base class for simple web checkers"""
     _request_log_filename: ClassVar[str] = "requests.log"
     _request_log_encoding: ClassVar[str] = "utf-8"
     _record_cell_delimiter: ClassVar[str] = ";"
@@ -46,16 +47,16 @@ class WebCheckerBase(FeedChecker):
                 return None
             return int(lines[-1].split(self._record_cell_delimiter)[1])
 
+    def log_request_status(self, request_log_path: str, status: int) -> None:
+        with open(request_log_path, "a", encoding=self._request_log_encoding) as file:
+            logger.debug("Writing status code to %s...", request_log_path)
+            file.write(f"{datetime.now().isoformat()}{self._record_cell_delimiter}{status}\n")
 
-class UrlAvailabilityChecker(FeedChecker):
-    _request_log_filename: ClassVar[str] = "requests.log"
-    _request_log_encoding: ClassVar[str] = "utf-8"
-    _record_cell_delimiter: ClassVar[str] = ";"
+
+class UrlAvailabilityChecker(WebCheckerBase):
 
     def __init__(self, email_client: EmailClient, http_client: HTTPClientBase, config: dict) -> None:
-        super().__init__(config)
-        self._email_client = email_client
-        self._http_client = http_client
+        super().__init__(http_client, email_client, config)
         self.logger = logging.getLogger("WebServiceAvailabilityChecker")
 
     def check(self) -> None:
@@ -66,7 +67,7 @@ class UrlAvailabilityChecker(FeedChecker):
         requests_log = os.path.join(
             data_dir, self._request_log_filename
         )
-        last_status_code = self._get_latest_status_code(requests_log)
+        last_status_code = self.get_last_request_status(requests_log)
         expected_status_code = self.config[ConfigKeys.EXPECTED_STATUS_CODE]
         logger.debug("Last status code: %s", last_status_code)
         if last_status_code == expected_status_code:
@@ -79,38 +80,16 @@ class UrlAvailabilityChecker(FeedChecker):
         url = self.config[ConfigKeys.URL]
         name = self.config[ConfigKeys.NAME]
         logger.debug("Checking availability of web service at %s...", url)
-        status_code = self._http_client.get_response_code(url)
+        status_code = self.http_client.get_response_code(url)
         if status_code == expected_status_code:
             subject = f"Web service {name} returns status code {status_code}"
             body = f"Web service at {url} is returning status code {status_code}"
             message = EmailMessage(subject=subject, body=body)
-            self._log_status_code(requests_log, status_code)
-            self._email_client.send_email(message)
-
-    def _get_latest_status_code(self, request_log_path: str) -> int | None:
-        if not os.path.exists(request_log_path):
-            logger.warning("Request log %s doesn't exist!", request_log_path)
-            return None
-
-        with open(request_log_path, "r", encoding=self._request_log_encoding) as file:
-            lines = file.readlines()
-            if not lines:
-                return None
-            return int(lines[-1].split(self._record_cell_delimiter)[1])
-
-    def _log_status_code(self, request_log_path: str, status_code: int) -> None:
-        with open(request_log_path, "a", encoding=self._request_log_encoding) as file:
-            logger.debug("Writing status code to %s...", request_log_path)
-            file.write(
-                f"{datetime.now().isoformat()}{self._record_cell_delimiter}{status_code}\n"
-            )
+            self.log_request_status(requests_log, status_code)
+            self.email_client.send_email(message)
 
 
 class PageContentChecker(WebCheckerBase):
-    _request_log_filename: ClassVar[str] = "requests.log"
-    _request_log_encoding: ClassVar[str] = "utf-8"
-    _record_cell_delimiter: ClassVar[str] = ";"
-    _html_parser: ClassVar[str] = "html.parser"
     _check_success: ClassVar[int] = 0
     _check_failed: ClassVar[int] = 1
 
@@ -142,7 +121,7 @@ class PageContentChecker(WebCheckerBase):
         response = self.http_client.get_response_string(url)
         if not response:
             self.logger.error("%s: Failed to get response from %s", name, url)
-            self._log_status_code(requests_log, self._check_failed)
+            self.log_request_status(requests_log, self._check_failed)
             return
 
         response_content_bs = BeautifulSoup(response, "html.parser")
@@ -150,16 +129,11 @@ class PageContentChecker(WebCheckerBase):
         if self._is_content_updated(str(html_node)):
             self.logger.info("Content updated. Saving content...")
             self._write_page_content(str(html_node))
-            self._log_status_code(requests_log, self._check_success)
-
-    def _log_status_code(self, request_log_path: str, status: int) -> None:
-        with open(request_log_path, "a", encoding=self._request_log_encoding) as file:
-            logger.debug("Writing status code to %s...", request_log_path)
-            file.write(f"{datetime.now().isoformat()}{self._record_cell_delimiter}{status}\n")
+            self.log_request_status(requests_log, self._check_success)
 
     def _write_page_content(self, page_content: str) -> None:
         file_path = os.path.join(self._content_dir_path, f"{datetime.now().isoformat()}.html")
-        with open(file_path, "w") as file:
+        with open(file_path, "w", encoding="utf-8") as file:
             logger.debug("Writing page content to file %s...", file_path)
             file.write(page_content)
 
