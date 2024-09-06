57769e699ef8e1566c2a3a26a9d4aeac325d0496
Daniel <d@stuhrs.dk>
2024-09-06 10:34:10 +0200

    Refactor

--- feeds/feed/rss.py
+++ feeds/feed/rss.py
@@ -32,29 +32,31 @@ class RSSFeedChecker(FeedChecker):
         self._http_client = http_client
         self._email_client = email_client
         self._logger = logging.getLogger("RSSFeedChecker")
+        self.data_dir_path = self.config[ConfigKeys.DIR]
+        self.saved_feeds_count = self.config[ConfigKeys.SAVED_FEEDS_COUNT]
+        self.url = self.config[ConfigKeys.URL]
+
 
     def check(self) -> None:
         try:
-            feed_dir = self.config[ConfigKeys.DIR]
-            if not os.path.exists(feed_dir):
-                os.mkdir(feed_dir)
+            if not os.path.exists(self.data_dir_path):
+                os.mkdir(self.data_dir_path)
 
-            url = self.config[ConfigKeys.URL]
-            feed = self._http_client.get_response_string(url)
+            feed = self._http_client.get_response_string(self.url)
             if not feed:
-                raise FeedCheckFailedError(f"Failed to download feed at {url}")
+                raise FeedCheckFailedError(f"Failed to download feed at {self.url}")
 
             rss_tree = ET.ElementTree(ET.fromstring(feed))
             if self._feed_content_updated(rss_tree):
                 self._logger.debug(
-                    "Feed %s updated. Saving feed...", self.config[ConfigKeys.NAME]
+                    "Feed %s updated. Saving feed...", self.name
                 )
                 self._save_feed(rss_tree)
                 self._send_notification_email(self._parse_feed_items(rss_tree))
                 self._remove_old_feeds()
         except Exception as ex:
             raise FeedCheckFailedError(
-                f"Error checking RSS feed {self.config[ConfigKeys.NAME]}: {ex}"
+                f"Error checking RSS feed {self.name}: {ex}"
             ) from ex
 
     def _parse_feed_items(self, tree: ET.ElementTree) -> list[RssItem]:
@@ -74,7 +76,7 @@ class RSSFeedChecker(FeedChecker):
             return True
 
         latest_feed = saved_feeds[0]
-        latest_saved_feed_path = os.path.join(self.config[ConfigKeys.DIR], latest_feed)
+        latest_saved_feed_path = os.path.join(self.data_dir_path, latest_feed)
         latest_saved_feed_tree = ET.parse(latest_saved_feed_path)
 
         channel_old_feed_bytes = b"".join(
@@ -92,17 +94,17 @@ class RSSFeedChecker(FeedChecker):
         return not hash_equals(channel_old_feed_bytes, channel_new_feed_bytes)
 
     def _save_feed(self, feed: ET.ElementTree) -> None:
-        feed_name = f"{self.config[ConfigKeys.NAME]}_{datetime.now().strftime('%Y-%m-%d_%H_%M')}.xml"
+        feed_name = f"{self.name}_{datetime.now().strftime('%Y-%m-%d_%H_%M')}.xml"
         self._logger.debug("Writing feed %s", feed_name)
-        feed.write(os.path.join(self.config[ConfigKeys.DIR], feed_name))
+        feed.write(os.path.join(self.data_dir_path, feed_name))
 
     def _send_notification_email(self, rss_items: Sequence[RssItem]) -> None:
-        subject = f"RSS-feed {self.config[ConfigKeys.NAME]} opdateret"
+        subject = f"RSS-feed {self.name} opdateret"
         rss_items_formatted = [
             (x.published_date, create_link(x.link, x.title)) for x in rss_items
         ]
 
-        html_heading = create_heading_two(self.config[ConfigKeys.NAME])
+        html_heading = create_heading_two(self.name)
         html_table = create_table(["Oprettet", "Link"], rss_items_formatted)
 
         body = f"{html_heading}\n{html_table}"
@@ -111,10 +113,10 @@ class RSSFeedChecker(FeedChecker):
 
     def _remove_old_feeds(self) -> None:
         saved_feeds = self._list_data_dir(descending=True)
-        if len(saved_feeds) > self.config[ConfigKeys.SAVED_FEEDS_COUNT]:
-            for feed in saved_feeds[self.config[ConfigKeys.SAVED_FEEDS_COUNT]:]:
+        if len(saved_feeds) > self.saved_feeds_count:
+            for feed in saved_feeds[self.saved_feeds_count:]:
                 self._logger.debug("Removing old feed %s...", feed)
-                os.remove(os.path.join(self.config[ConfigKeys.DIR], feed))
+                os.remove(os.path.join(self.data_dir_path, feed))
 
     def _list_data_dir(self, descending: bool) -> list[str]:
-        return sorted(os.listdir(self.config[ConfigKeys.DIR]), reverse=descending)
+        return sorted(os.listdir(self.data_dir_path), reverse=descending)
