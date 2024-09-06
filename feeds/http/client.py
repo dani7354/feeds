01cbc777f35401890a58f93d62f01ee7533684b4
Daniel <d@stuhrs.dk>
2024-09-06 20:39:05 +0200

    Add HTTPClient for dynamic content (loaded using AJAX calls) based on selenium

--- feeds/http/client.py
+++ feeds/http/client.py
@@ -1,5 +1,10 @@
 import requests
 from requests import Response
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class HTTPClientBase:
@@ -12,20 +17,35 @@ class HTTPClientBase:
         raise NotImplementedError
 
 
+class HTTPClientDynamicBase:
+    def get_content_by_css_selector(
+            self,
+            url: str,
+            css_selector_loaded: str,
+            css_selector_content: str) -> str:
+        """
+        Should be overwritten by subclasses
+        url: str: URL to get content from
+        css_selector_loaded: str: CSS selector to wait for before getting content
+        css_selector_content: str: CSS selector to get content from
+        """
+        raise NotImplementedError
+
+
 class HTTPClient(HTTPClientBase):
-    def __init__(self, headers: dict):
+    def __init__(self, headers: dict[str, str]) -> None:
         self._headers = headers
-        self._timeout = 60
+        self._timeout_seconds = 60
 
     def get_response_string(self, url: str) -> str:
-        response = requests.get(url, headers=self._headers, timeout=self._timeout)
+        response = requests.get(url, headers=self._headers, timeout=self._timeout_seconds)
         if response.status_code != 200:
             return ""
 
         return self._decode_response(response)
 
     def get_response_code(self, url: str) -> int:
-        response = requests.get(url, headers=self._headers, timeout=self._timeout)
+        response = requests.get(url, headers=self._headers, timeout=self._timeout_seconds)
         return response.status_code
 
     @staticmethod
@@ -37,3 +57,25 @@ class HTTPClient(HTTPClientBase):
             if "ISO-8859-1" in content_type_header_value:
                 return response.content.decode(encoding="latin-1")
             raise
+
+
+class HTTPClientDynamic(HTTPClientDynamicBase):
+    def __init__(self, headers: dict[str, str]) -> None:
+        self._headers = headers
+        self._timeout_seconds = 10
+
+    def get_content_by_css_selector(
+            self,
+            url: str,
+            css_selector_loaded: str,
+            css_selector_content) -> str:
+        driver_options = Options()
+        driver_options.add_argument("--headless")
+        with Firefox(options=driver_options) as driver:
+            driver.get(url)
+            _ = WebDriverWait(driver, timeout=self._timeout_seconds).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, css_selector_loaded))
+            )
+            content_html_element = driver.find_element(By.CSS_SELECTOR, css_selector_content)
+
+            return content_html_element.text
