14e123edba7876510dc6774e42b2ad77f35a75e3
Daniel <d@stuhrs.dk>
2025-01-13 06:23:33 +0100

    Fix enc message format

--- feeds/email/client.py
+++ feeds/email/client.py
@@ -2,6 +2,8 @@ import dataclasses
 import smtplib
 import ssl
 from email.header import Header
+from email.message import Message
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from enum import StrEnum
 from typing import Sequence
@@ -49,14 +51,14 @@ class StandardSMTP(EmailClient):
     encoding = "utf-8"
 
     def send_email(self, email: EmailMessage) -> None:
-        mime_message = self._create_message_str(email)
+        mime_message = self._create_message(email)
         context = ssl.create_default_context()
         with smtplib.SMTP(host=self.configuration.smtp_host, port=self.configuration.smtp_port) as mail_server:
             mail_server.starttls(context=context)
             mail_server.login(self.configuration.smtp_user, self.configuration.smtp_password)
             mail_server.send_message(mime_message)
 
-    def _create_message_str(self, message: EmailMessage) -> MIMEText:
+    def _create_message(self, message: EmailMessage) -> MIMEBase:
         mime_text_message = MIMEText(message.body, "html", self.encoding)
         mime_text_message[MimeMessageField.SUBJECT] = Header(message.subject, self.encoding)
         mime_text_message[MimeMessageField.FROM] = self.configuration.sender
@@ -72,16 +74,27 @@ class EncryptedEmailClient(StandardSMTP):
         super().__init__(configuration)
         self._pgp_service = pgp_service
 
-    def _create_message_str(self, message: EmailMessage) -> MIMEText:
-        mime_text_message = MIMEText(
-            self._pgp_service.encrypt_string(message.body, recipient=self.configuration.recipients[0]),
-            "html",
-            self.encoding)
-        mime_text_message[MimeMessageField.SUBJECT] = Header(message.subject, self.encoding)
-        mime_text_message[MimeMessageField.FROM] = self.configuration.sender
-        mime_text_message[MimeMessageField.TO] = self.configuration.recipients[0]
+    def _create_message(self, message: EmailMessage) -> MIMEBase:
+        mime_message = MIMEBase(_maintype="multipart", _subtype="encrypted", protocol="application/pgp-encrypted")
+        mime_message.add_header(_name="Content-Type", _value="multipart/mixed", protected_headers="v1")
+        mime_message[MimeMessageField.FROM] = self.configuration.sender
+        mime_message[MimeMessageField.TO] = self.configuration.recipients[0]
 
-        return mime_text_message
+        pgp_version_info_message = Message()
+        pgp_version_info_message.add_header(_name="Content-Type", _value="application/pgp-encrypted")
+        pgp_version_info_message.add_header(_name="Content-Description", _value="PGP/MIME version identification")
+        pgp_version_info_message.set_payload("Version: 1" + "\n")
+
+        pgp_payload = Message()
+        pgp_payload.add_header(_name="Content-Type", _value="application/octet-stream", name="encrypted.asc")
+        pgp_payload.add_header(_name="Content-Description", _value="OpenPGP encrypted message")
+        pgp_payload.add_header(_name="Content-Disposition", _value="inline", filename="encrypted.asc")
+        pgp_payload.set_payload(self._pgp_service.encrypt_string(message.body, self.configuration.recipients[0]))
+
+        mime_message.attach(pgp_version_info_message)
+        mime_message.attach(pgp_payload)
+
+        return mime_message
 
 
 class DummyEmailClient(EmailClient):
