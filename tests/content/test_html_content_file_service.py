dd1e912be5ed3d0eaf009a19c3e948dae3ace01a
Daniel <d@stuhrs.dk>
2025-01-23 05:55:14 +0100

    Add tests

--- tests/content/test_html_content_file_service.py
+++ tests/content/test_html_content_file_service.py
@@ -8,7 +8,15 @@ def html_file_service(tmp_path) -> HtmlContentFileService:
     return HtmlContentFileService(str(tmp_path), "my_page")
 
 
-def test_get_diff_different_html_files(html_file_service):
+def count_added_lines(diff: str) -> int:
+    return len([x for x in diff.splitlines(keepends=False) if x.startswith("+ ")])
+
+
+def count_removed_lines(diff: str) -> int:
+    return len([x for x in diff.splitlines(keepends=False) if x.startswith("- ")])
+
+
+def test_get_diff_new_content(html_file_service):
     latest_saved_file_content = """
     <html>
         <body>
@@ -30,8 +38,36 @@ def test_get_diff_different_html_files(html_file_service):
     </html>
     """
     diff = html_file_service.get_diff(new_content)
-    assert diff == ""
 
+    assert count_added_lines(diff) == 4
+    assert count_removed_lines(diff) == 0
+
+
+def test_get_diff_removed_content(html_file_service):
+    latest_saved_file_content = """
+    <html>
+        <head>
+            <title>Test</title>
+        </head>
+        <body>
+            <h1>Test</h1>
+            <p>Some new paragraph...</p>
+        </body>
+    </html>
+    """
+    html_file_service.save_content(latest_saved_file_content.encode())
+
+    new_content = """
+    <html>
+        <body>
+            <h1>Test</h1>
+        </body>
+    </html>
+    """
+    diff = html_file_service.get_diff(new_content)
+
+    assert count_removed_lines(diff) == 4
+    assert count_added_lines(diff) == 0
 
 def test_get_diff_first_content_file(html_file_service):
     new_content = """
@@ -46,7 +82,9 @@ def test_get_diff_first_content_file(html_file_service):
     </html>
     """
     diff = html_file_service.get_diff(new_content)
-    assert diff == new_content
+
+    assert count_added_lines(diff) == 10
+    assert count_removed_lines(diff) == 0
 
 
 def test_get_diff_same_html_files(html_file_service):
@@ -60,13 +98,9 @@ def test_get_diff_same_html_files(html_file_service):
     """
     html_file_service.save_content(latest_saved_file_content.encode())
 
-    new_content = """
-    <html>
-        <body>
-            <h1>Test</h1>
-            <p>Some new paragraph...</p>
-        </body>
-    </html>
-    """
+    new_content = latest_saved_file_content
     diff = html_file_service.get_diff(new_content)
+
     assert diff == ""
+    assert count_added_lines(diff) == 0
+    assert count_removed_lines(diff) == 0
